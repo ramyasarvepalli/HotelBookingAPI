4cc56a99c1cf5aabddbcc2256c115675728de1e1
ramya sarvepalli <ramya.sarvepalli@gmail.com>
2025-01-24 22:42:05 +0530

    Add project files.

---
+++ Controllers/HotelBookingController.cs
@@ -0,0 +1,33 @@
+﻿using HotelBookingAPI.Data;
+using HotelBookingAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public HotelBookingController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult CreateEdit(HotelBooking hotel)
+        {
+            if (hotel.Id == 0)
+            {
+                // Create new hotel booking
+            }
+            else
+            {
+                // Update existing hotel booking
+            }
+            return Ok(hotel);
+        }
+    }
+}
---
+++ Data/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using HotelBookingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext:DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
+        public DbSet<HotelBooking> HotelBookings { get; set; }
+    }
+}
